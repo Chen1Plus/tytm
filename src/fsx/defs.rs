64a8cacb510608d0aa2979e607b6765dd06fd655
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-08-01 16:33:09 +0800

    refactor: use lazy lock

--- src/fsx/defs.rs
+++ src/fsx/defs.rs
@@ -1,38 +1,38 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::LazyLock};
 
 use super::ensure_dir;
 
+/// The user's data directory.
+static DATA: LazyLock<PathBuf> =
+    LazyLock::new(|| dirs::data_dir().expect("Failed to find user's data directory"));
+/// The user's cache directory.
+static CACHE: LazyLock<PathBuf> =
+    LazyLock::new(|| dirs::cache_dir().expect("Failed to find user's cache directory"));
+
 #[cfg(debug_assertions)]
-lazy_static::lazy_static! {
-    /// The user's data directory.
-    static ref DATA: PathBuf = dirs::data_dir().expect("Failed to find user's data directory");
+static TYPORA: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from("debug-dirs").join("Typora"));
+#[cfg(all(not(debug_assertions), target_os = "windows"))]
+static TYPORA: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("Typora"));
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+static TYPORA: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("abnerworks.Typora"));
 
-    static ref TYPORA: PathBuf = DATA.join("Typora");
-    pub(crate) static ref TYPORA_THEME: PathBuf = PathBuf::from("debug-dirs").join("typora-themes");
-    pub(crate) static ref TYPORA_MANIFEST: PathBuf = TYPORA_THEME.join("tytm-pkgs");
+pub(crate) static TYPORA_THEME: LazyLock<PathBuf> = LazyLock::new(|| TYPORA.join("themes"));
+pub(crate) static TYPORA_MANIFEST: LazyLock<PathBuf> =
+    LazyLock::new(|| TYPORA_THEME.join("tytm-pkgs"));
 
-    static ref TYTM: PathBuf = DATA.join("tytm");
-    pub(crate) static ref TYTM_MANIFEST: PathBuf = PathBuf::from("manifest");
-}
+static TYTM: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("tytm"));
+
+#[cfg(debug_assertions)]
+pub(crate) static TYTM_MANIFEST: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from("manifest"));
+#[cfg(not(debug_assertions))]
+pub(crate) static TYTM_MANIFEST: LazyLock<PathBuf> = LazyLock::new(|| TYTM.join("manifest"));
 
 #[cfg(debug_assertions)]
 pub(crate) fn init() {
     ensure_dir("debug-dirs").unwrap();
+    ensure_dir(TYPORA.as_path()).unwrap();
     ensure_dir(TYPORA_THEME.as_path()).unwrap();
     ensure_dir(TYPORA_MANIFEST.as_path()).unwrap();
-}
-
-#[cfg(all(not(debug_assertions), target_os = "windows"))]
-lazy_static::lazy_static! {
-    /// The user's data directory.
-    static ref DATA: PathBuf = dirs::data_dir().expect("Failed to find user's data directory");
-
-    static ref TYPORA: PathBuf = DATA.join("Typora");
-    pub(crate) static ref TYPORA_THEME: PathBuf = TYPORA.join("themes");
-    pub(crate) static ref TYPORA_MANIFEST: PathBuf = TYPORA_THEME.join("tytm-pkgs");
-
-    static ref TYTM: PathBuf = DATA.join("tytm");
-    pub(crate) static ref TYTM_MANIFEST: PathBuf = TYTM.join("manifest");
 }
 
 #[cfg(not(debug_assertions))]
@@ -54,17 +54,4 @@ pub(crate) fn init() {
 
     ensure_dir(TYTM_MANIFEST.as_path()).expect("Failed to create TyTM manifest directory");
     assert!(TYTM_MANIFEST.is_dir());
-}
-
-#[cfg(all(not(debug_assertions), target_os = "macos"))]
-lazy_static::lazy_static! {
-    /// The user's data directory.
-    static ref DATA: PathBuf = dirs::data_dir().expect("Failed to find user's data directory");
-
-    static ref TYPORA: PathBuf = DATA.join("abnerworks.Typora");
-    pub(crate) static ref TYPORA_THEME: PathBuf = TYPORA.join("themes");
-    pub(crate) static ref TYPORA_MANIFEST: PathBuf = TYPORA_THEME.join("tytm-pkgs");
-
-    static ref TYTM: PathBuf = DATA.join("tytm");
-    pub(crate) static ref TYTM_MANIFEST: PathBuf = TYTM.join("manifest");
 }
