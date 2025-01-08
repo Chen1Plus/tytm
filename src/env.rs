060acd102b47529497cfdfce83c494ce730a049e
Chen1Plus <122733139+Chen1Plus@users.noreply.github.com>
2025-01-08 11:32:55 +0800

    rm unused env var

--- src/env.rs
+++ src/env.rs
@@ -13,27 +13,19 @@ static TYPORA: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("Typora"));
 #[cfg(all(not(debug_assertions), target_os = "macos"))]
 static TYPORA: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("abnerworks.Typora"));
 
-pub(crate) static TYPORA_THEME: LazyLock<PathBuf> = LazyLock::new(|| TYPORA.join("themes"));
-pub(crate) static TYPORA_MANIFEST: LazyLock<PathBuf> =
-    LazyLock::new(|| TYPORA_THEME.join("tytm-pkgs"));
+pub static TYPORA_THEME: LazyLock<PathBuf> = LazyLock::new(|| TYPORA.join("themes"));
 
 static TYTM: LazyLock<PathBuf> = LazyLock::new(|| DATA.join("tytm"));
 
 #[cfg(debug_assertions)]
-pub(crate) static TYTM_MANIFEST: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from("manifest"));
-#[cfg(not(debug_assertions))]
-pub(crate) static TYTM_MANIFEST: LazyLock<PathBuf> = LazyLock::new(|| TYTM.join("manifest"));
-
-#[cfg(debug_assertions)]
-pub(crate) fn init() {
+pub fn init() {
     ensure_dir("debug-dirs").unwrap();
     ensure_dir(TYPORA.as_path()).unwrap();
     ensure_dir(TYPORA_THEME.as_path()).unwrap();
-    ensure_dir(TYPORA_MANIFEST.as_path()).unwrap();
 }
 
 #[cfg(not(debug_assertions))]
-pub(crate) fn init() {
+pub fn init() {
     assert!(
         TYPORA.exists() && TYPORA.is_dir(),
         "Typora directory not found"
@@ -43,14 +35,8 @@ pub(crate) fn init() {
         "Typora themes directory not found"
     );
 
-    ensure_dir(TYPORA_MANIFEST.as_path()).expect("Failed to create Typora manifest directory");
-    assert!(TYPORA_MANIFEST.is_dir());
-
     ensure_dir(TYTM.as_path()).expect("Failed to create TyTM directory");
     assert!(TYTM.is_dir());
-
-    ensure_dir(TYTM_MANIFEST.as_path()).expect("Failed to create TyTM manifest directory");
-    assert!(TYTM_MANIFEST.is_dir());
 }
 
 /// Ensure that a directory exists, failed if missing parent directories.
